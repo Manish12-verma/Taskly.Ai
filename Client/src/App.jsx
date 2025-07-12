ebbe57587fe914ce7bb358ea424b3b32a3d341d8
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-12 12:43:05 +0530

    added routing for all pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,34 @@
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import WriteArticle from "./pages/WriteArticle";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import BlogTitles from "./pages/BlogTitles";
+import GenerateImages from "./pages/GenerateImages";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+           <Route path="remove-background" element={<RemoveBackground />} />
+           <Route path="remove-object" element={<RemoveObject />} />
+           <Route path="review-resume" element={<ReviewResume />} />
+           <Route path="community" element={<Community />} />
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Client/src/pages/RemoveBackground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RemoveBackground = () => {
+  return (
+    <div>
+        
+      Remove bg
+    </div>
+  )
+}
+
+export default RemoveBackground
