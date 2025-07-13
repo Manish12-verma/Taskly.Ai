c4a6d3ae98f6d6b6c80d0d0d5710cd7a0c4d4ea0
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2025-07-13 12:44:48 +0530

    fixed navbar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const { openSignIn } = useClerk();
 
   return (
-    <div className=" fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer">
+    <div className=" fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-30 cursor-pointer">
       <img
         src={assets.logo}
         alt="logo"
